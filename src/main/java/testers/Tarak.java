8b2ed5ad9909fa60b066099c30db039b89c212d7
meaht9 <meaht9@gmail.com>
2019-10-28 21:19:25 -0400

    My code for past 2 weeks

--- src/main/java/testers/Tarak.java
+++ src/main/java/testers/Tarak.java
@@ -1,5 +1,7 @@
 package testers;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -20,13 +22,25 @@ public class Tarak {
     @Test
     public void OpenChrome() {
         ChromeDriver driver = new ChromeDriver();
-        String google = "https://google.com";
+        String google = "https://facebook.com";
 
         driver.get(google);
 
         String title = driver.getTitle();
 
         String url = driver.getCurrentUrl();
+
+        // found elements through locator for facebook
+        //located the email box and password box on  findElement in the next four lines
+
+
+       WebElement email = driver.findElement(By.name("email"));
+       WebElement pass = driver.findElement(By.name("pass"));
+
+       email.sendKeys("email@gmail.com");
+       email.sendKeys("thisismypassword");
+
+
 
         System.out.println("My First Automation is going to " + title + " and the link for it is " + url);
 
---
+++ src/main/java/testers/TarakPractice.java
@@ -0,0 +1,30 @@
+package testers;
+
+import java.util.Scanner;
+
+public class TarakPractice {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter an age: ");
+        Scanner age = new Scanner(System.in);
+        int userage = age.nextInt();
+
+
+        if (userage >= 0 && userage <= 5) {
+            System.out.println("Baby");
+        } else if (userage >= 6 && userage <= 11) {
+            System.out.println("Kid");
+        } else if (userage >= 12 && userage <=17) {
+            System.out.println("Teen");
+        } else if (userage >=18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Invalid");
+        }
+
+
+    }
+
+
+}
