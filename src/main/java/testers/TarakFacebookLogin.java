17f4d6b1a345007ec46c57695f9b16b04b56ced7
meaht9 <meaht9@gmail.com>
2019-11-05 16:38:26 -0500

    comment

--- src/main/java/testers/TarakFacebookLogin.java
+++ src/main/java/testers/TarakFacebookLogin.java
@@ -1,36 +1,39 @@
 package testers;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Tarak  {
+public class TarakFacebookLogin {
 
     @BeforeTest
-    public void SetupChrome()  {
+    public void SetupChrome () {
         ChromeOptions co = new ChromeOptions();
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         capabilities.setCapability(ChromeOptions.CAPABILITY, co);
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-
     }
 
     @Test
-    public void login() {
+    public void login () {
         ChromeDriver driver = new ChromeDriver();
-        String url = "http://the-internet.herokuapp.com/login";
-
+        String url = "https://facebook.com";
         driver.get(url);
 
-        String username = "tomsmith";
-        String password = "SuperSecretPassword!";
+        String username = "123";
+        String password = "123";
 
+        driver.findElement(By.name("email")).sendKeys(username);
+        driver.findElement(By.name("pass")).sendKeys(password);
 
-
-
+        driver.findElement(By.id("u_0_2")).click();
 
     }
+
+
+
 
 }
